d544fe138ea705ad5c208e37053c425f067d82cf
Jiachen FENG <fengjiachen@github.com>
2019-10-26 22:26:25 +0800

    166 divide ans to str

--- 150_199/166. Fraction to Recurring Decimal/main.cpp
+++ 150_199/166. Fraction to Recurring Decimal/main.cpp
@@ -15,7 +15,47 @@
 // Input: numerator = 2, denominator = 3
 // Output: "0.(6)"
 #include <iostream>
+#include <map>
+#include <string>
+
 using namespace std;
+string fractionToDecimal(int numerator, int denominator)
+{
+    if (numerator == 0)
+        return "0";
+    string ans;
+    if ((numerator < 0) ^ (denominator < 0))
+        ans += '-';
+    long long n = numerator;
+    n = abs(n);
+    long long d = denominator;
+    d = abs(d);
+    long long integer = n / d;
+    ans += to_string(integer);
+    long long r = n % d;
+    if (r != 0)
+        ans += '.';
+    else
+        return ans;
+    map<long long, int> m;
+    while (r)
+    {
+        if (m.count(r))
+        {
+            ans.insert(m[r], "(");
+            ans += ')';
+            break;
+        }
+        m[r] = ans.size();
+        r *= 10;
+        ans += to_string(r / d);
+        r = r % d;
+    }
+    return ans;
+}
 int main()
 {
+    int n = 9;
+    int d = 3;
+    cout << fractionToDecimal(d, n) << endl;
 }
